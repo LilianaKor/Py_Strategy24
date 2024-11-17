02ed72aeaa2fe24b2662e356e0414f819ff89b74
Liliana <lilianakoric@gmail.com>
2024-11-16 21:20:54 -0800

    Create structure for the project, - add def test_upload(self, driver) pass

--- pages/upload_page.py
+++ pages/upload_page.py
@@ -4,5 +4,5 @@ from locators.upload_page_herokuapp_locators import UploadPageLocators
 class UploadPage(BasePage):
     locators = UploadPageLocators
 
-    def upload_file(self):
-        self.element_is_visible(self.locators.UPLOAD_loc).send_keys("C:\\Users\\User\\Desktop\\test.txt")
+    def upload_file(self, file_path):
+        self.element_is_visible(self.locators.UPLOAD_loc).send_keys(file_path)
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,3 +1,5 @@
+import time
+
 from data.urls import Urls
 from functions import get_root_path
 from pages.upload_page import UploadPage
@@ -10,4 +12,5 @@ class TestUpload:
         file_path = get_root_path("data/upload/bb.txt")
         page = UploadPage(driver, f"{self.url.herokuapp_base_url}upload")
         page.open()
-        page.upload_file()
+        page.upload_file(file_path)
+        time.sleep(5)
