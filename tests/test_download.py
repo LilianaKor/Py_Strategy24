d95a3018bb3b01c253171dd1a41fd87310ba9311
Liliana <lilianakoric@gmail.com>
2024-11-16 19:24:27 -0800

    Create structure for the project, add urls.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,9 @@
+from pages.download_page import DownloadPage
+from data.urls import Urls
 
 
 class TestDownload:
+    url = Urls()
 
-    def test_download(self,driver):
-        pass
+    def test_download(self, driver):
+        page = DownloadPage(driver, f"{self.url.herokubase_url}/download")
