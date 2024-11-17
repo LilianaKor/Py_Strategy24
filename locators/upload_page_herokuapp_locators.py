bec39d6db2f6869b40df8e26c1497dcf76455a31
Liliana <lilianakoric@gmail.com>
2024-11-16 20:55:31 -0800

    Create structure for the project, - add options.add_experimental_option

---
+++ locators/upload_page_herokuapp_locators.py
@@ -0,0 +1,5 @@
+class UploadPageLocators:
+    UPLOAD_loc = ("xpath", "//input[@id='file-upload']")
+    HEADER_TEXT_loc = ("xpath", "//div[@class='example']//h3")
+    UPLOADED_FILE_loc = ("xpath", "//div[@id='uploaded-files']")
+
