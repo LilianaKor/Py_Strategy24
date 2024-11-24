781784f4313f5b8258a70507ce0b414998f1a3b5
Liliana <lilianakoric@gmail.com>
2024-11-24 12:14:12 -0800

    Create structure for the project, - add text_tool_tips.py pass

--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,4 @@
 class Urls:
-
     herokuapp_base_url = "https://the-internet.herokuapp.com/"
     demoqa_buttons_url = "https://demoqa.com/buttons"
     demoqa_tool_tips_url = "https://demoqa.com/tool-tips"
