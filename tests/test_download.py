f589e6e68c421ee8c4c4ed53e5c4487894d126a0
Liliana <lilianakoric@gmail.com>
2025-04-06 11:04:22 -0700

    ADD  test_drag_and_drop and test_hover

--- tests/test_download.py
+++ tests/test_download.py
@@ -23,3 +23,24 @@ def test_dropdown():
     driver.find_element(By.XPATH, "//option[@value='1']").click()
     assert driver.find_element(By.ID, 'dropdown').get_attribute('value') == '1'
     driver.quit()
+
+
+def test_drag_and_drop():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/drag_and_drop")
+    box1 = driver.find_element(By.ID, 'column-a')
+    box2 = driver.find_element(By.ID, 'column-b')
+    actions = ActionChains(driver)
+    actions.drag_and_drop(box1, box2).perform()
+    time.sleep(3)
+    assert driver.find_element(By.ID, 'column-a').text == 'B'
+    driver.quit()
+
+
+def test_hovers():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/hovers")
+    driver.find_element(By.XPATH, "//div[@class='figure'][1]").click()
+    time.sleep(3)
+    assert driver.find_element(By.XPATH, "//h5[contains(text(), 'name: user1')]").is_displayed()
+    driver.quit()
